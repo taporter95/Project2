9435211b3002bf8117555eb2526ba98bd6e2da3b
Jason de Bernard <d105@umbc.edu>
2015-12-02 16:22:48 -0500

    Update AdminProcessCreateNew.php
    Eliminated session variables and added advising locations.

--- AdminProcessCreateNew.php
+++ AdminProcessCreateNew.php
@@ -1,20 +1,64 @@
 <?php
-session_start();
-
-$_SESSION["AdvF"] = $_POST["firstN"];
-$_SESSION["AdvL"] = $_POST["lastN"];
-$_SESSION["AdvUN"] = $_POST["UserN"];
-$_SESSION["AdvPW"] = $_POST["PassW"];
-$_SESSION["AdvOL"] = $_POST["OfficeLoc"];
-$_SESSION["PassCon"] = false;
-
-//check if password and confirm password match
-if($_POST["PassW"] == $_POST["ConfP"]){
-	header('Location: AdminCreateNew.php');
-}
-elseif($_POST["PassW"] != $_POST["ConfP"]){
-	$_SESSION["PassCon"] = true;
-	header('Location: AdminCreateNewAdv.php');
-}
-
+  session_start();
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <title>Create New Admin</title>
+    <script type="text/javascript">
+    function saveValue(target){
+	var stepVal = document.getElementById(target).value;
+	alert("Value: " + stepVal);
+    }
+    </script>
+    <link rel='stylesheet' type='text/css' href='../css/standard.css'/>
+  </head>
+  <body>
+    <div id="login">
+      <div id="form">
+        <div class="top">
+		<h2>New Advisor has been created:</h2>
+
+		<?php
+
+		if($_POST["PassW"] != $_POST["ConfP"]){
+			echo "<h3 style='color:red'>Passwords do not match!!</h3>";
+			header('Location: AdminCreateNewAdv.php');
+		}
+
+			$first = $_POST["firstN"];
+			$last = $_POST["lastN"];
+			$user = $_POST["UserN"];
+			$pass = $_POST["PassW"];
+      			$Office = $_POST["OfficeLoc"];
+			$Location = $_POST["AppointLoc"];
+
+			include('../CommonMethods.php');
+			$debug = false;
+			$Common = new Common($debug);
+
+      $sql = "SELECT * FROM `Proj2Advisors` WHERE `Username` = '$user' AND `FirstName` = '$first' AND  `LastName` = '$last'";
+      $rs = $Common->executeQuery($sql, "Advising Appointments");
+      $row = mysql_fetch_row($rs);
+      if($row){
+        echo("<h3>Advisor $first $last already exists</h3>");
+      }
+      else{
+  			$sql = "INSERT INTO `Proj2Advisors`(`FirstName`, `LastName`, `Username`, `Password`, `Location`, `Office`) 
+  			VALUES ('$first', '$last', '$user', '$pass', '$Location', '$Office')";
+        echo ("<h3>$first $last<h3>");
+        $rs = $Common->executeQuery($sql, "Advising Appointments");
+      }
+		?>
+		<form method="link" action="AdminUI.php">
+			<input type="submit" name="next" class="button large go" value="Return to Home">
+		</form>
+	</div>
+	</div>
+	</div>
+	</form>
+  </body>
+  
+</html>
