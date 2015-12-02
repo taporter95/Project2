91ab937ed10e71f5771b96c248fce605df310fc3
Jason de Bernard <d105@umbc.edu>
2015-12-02 17:00:39 -0500

    Update 07StudSelectAdvisor.php
    changed row value

--- 07StudSelectAdvisor.php
+++ 07StudSelectAdvisor.php
@@ -25,7 +25,7 @@ $COMMON = new Common($debug);
 			$rs = $COMMON->executeQuery($sql, $_SERVER["SCRIPT_NAME"]);
 			while($row = mysql_fetch_row($rs)){
 				echo "<label for='",$row[0],"'><input id='",$row[0],"' type='radio' name='advisor' required value='", $row[0],"'>", $row[1]," ", $row[2],"<br>";
-				echo "Appointment Location: ",$row[5],"</label>";
+				echo "Appointment Location: ",$row[6],"</label>";
 			}
 		?>
         </div>
