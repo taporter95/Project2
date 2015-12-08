10748b924d225019409e18e990fe63d642dbf3e9
RyanErnst <rernst1@umbc.edu>
2015-12-08 10:02:30 -0500

    Update 09StudSearchApp.php
    Reconfigured the call to the css from "../css/standard.css" to "./css/standard.css" in order to get all GUI changes.

--- 09StudSearchApp.php
+++ 09StudSearchApp.php
@@ -9,7 +9,7 @@ $COMMON = new Common($debug);
   <head>
     <meta charset="UTF-8" />
     <title>Search for Appointment</title>
-    <link rel='stylesheet' type='text/css' href='../css/standard.css'/>
+    <link rel='stylesheet' type='text/css' href='./css/standard.css'/>
 
   </head>
   <body>
