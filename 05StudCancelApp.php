1c32a2742ab841f6b427f1bf413ad21652d62fc7
RyanErnst <rernst1@umbc.edu>
2015-12-08 10:00:32 -0500

    Update 05StudCancelApp.php
    Reconfigured the call to the css from "../css/standard.css" to "./css/standard.css" in order to get all GUI changes.

--- 05StudCancelApp.php
+++ 05StudCancelApp.php
@@ -9,7 +9,7 @@ $COMMON = new Common($debug);
   <head>
     <meta charset="UTF-8" />
     <title>Cancel Appointment</title>
-    <link rel='stylesheet' type='text/css' href='../css/standard.css'/>
+    <link rel='stylesheet' type='text/css' href='./css/standard.css'/>
   </head>
   <body>
     <div id="login">
