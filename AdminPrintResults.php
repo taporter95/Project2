cb93d20140c4d4bf2d896d9e2d8e7e35d1147232
RyanErnst <rernst1@umbc.edu>
2015-12-08 10:57:45 -0500

    GUI updated
    Reconfigured the call to the css from "../css/standard.css" to "./css/standard.css" in order to get all GUI changes.

--- AdminPrintResults.php
+++ AdminPrintResults.php
@@ -14,7 +14,7 @@ $debug = false;
 	alert("Value: " + stepVal);
     }
     </script>
-	<link rel='stylesheet' type='text/css' href='../css/standard.css'/>
+	<link rel='stylesheet' type='text/css' href='./css/standard.css'/>
   </head>
   <body>
     <div id="login">
